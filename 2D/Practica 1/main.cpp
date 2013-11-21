f53434c7af690f169bd62b19ee977c39f582d71b
David Escalona <davidkanc@gmail.com>
2013-11-21 18:01:41 +0100

    practica 2 2d

--- 2D/Practica 1/main.cpp
+++ 2D/Practica 1/main.cpp
@@ -6,7 +6,14 @@ int main(int argc, char* argv[]) {
 	Screen::Instance().Open(800, 600, false);
 	
 	while ( Screen::Instance().IsOpened() ) {
-		// TAREA: Pintar primitivas
+		
+		Renderer::Instance().Clear(255,0,0);
+
+		Renderer::Instance().SetColor(223,155,67,0);
+		Renderer::Instance().DrawRect(16,16,768,568);
+
+		Renderer::Instance().SetColor(123,55,67,0);
+		Renderer::Instance().DrawEllipse(400,150,260,100);
 		
 		// Refrescamos la pantalla
 		Screen::Instance().Refresh();

50bb8de0eec51a95f1fd5a26cc4315c9d922e492
kanc <davidkanc@gmail.com>
2014-01-03 17:28:17 +0100

    ani

--- 2D/Practica 6 - Animaciones/main.cpp
+++ 2D/Practica 6 - Animaciones/main.cpp
@@ -12,7 +12,8 @@ int main(int argc, char* argv[])
 	Map* mymap = new Map("data/map2.tmx",1);
 	Image* back = ResourceManager::Instance().LoadImage("data/back2.png");
 	Image* front = ResourceManager::Instance().LoadImage("data/back1.png");
-	Image* alien =  ResourceManager::Instance().LoadImage("data/alien.png");	
+	Image* alien =  ResourceManager::Instance().LoadImage("data/alienanim.png",8,1);	
+	alien->SetMidHandle();
 
 	MapScene* scene = new MapScene(mymap,back,front);
 	scene->SetAutoBackSpeed(-5,0);
@@ -22,15 +23,18 @@ int main(int argc, char* argv[])
 
 	Sprite* sprAlien = scene->CreateSprite(alien);
 	sprAlien->SetPosition(10,10);
-	sprAlien->SetColor(255,0,0,255);
+	sprAlien->SetFPS(16);
+	sprAlien->SetCurrentFrame(1);
+	sprAlien->SetFrameRange(1,20);
 	sprAlien->SetCollisionPixelData(new CollisionPixelData("data/aliencol.png"));	
 	sprAlien->SetCollision(Sprite::CollisionMode::COLLISION_RECT);
+	sprAlien->SetScale(3,3);	
 
 	scene->GetCamera().FollowSprite(sprAlien);
 	scene->GetCamera().SetBounds(0,0,mymap->GetWidth(), mymap->GetHeight());
 
 	bool jump = false;
-	float vel = 0, grav = 10, jumpTime = 0;
+	float vel = 0, grav = 10;
 
 	while ( screen.IsOpened() && !screen.KeyPressed(GLFW_KEY_ESC) ) 
 	{		
@@ -43,27 +47,25 @@ int main(int argc, char* argv[])
         else if ( screen.KeyPressed(GLFW_KEY_RIGHT) ) 
 			destX = sprAlien->GetX() + 10;
 
-		sprAlien->MoveTo(destX, sprAlien->GetY() - vel + grav, 200, 512);
+		sprAlien->MoveTo(destX, sprAlien->GetY() - vel + grav, 200, 500);
 		float distance = mymap->GetGroundY(sprAlien->GetX(), sprAlien->GetY()) -  (sprAlien->GetY() + sprAlien->GetImage()->GetHeight());
 
-		if ((-vel + grav)  > 0 && distance < 10) sprAlien->SetY(sprAlien->GetY() + distance);
+		screen.SetTitle(String::FromFloat(distance));
+
+		if ((-vel + grav) > 0 && distance < 13) sprAlien->SetY(sprAlien->GetY() + distance);
 
 		if ( screen.KeyPressed(GLFW_KEY_SPACE))
-		{	if (jumpTime < 0.5)
-			{
-				jump = true;
-				vel = 15;
-			}
+		{				
+			jump = true;
+			vel = 15;			
 		}
 			
 		if (jump)
-		{			
-			jumpTime+=screen.ElapsedTime();
-
+		{						
 			vel-= grav * screen.ElapsedTime();
+
 			if (vel <= 0)
-			{	jump = false;
-				jumpTime = 0;
+			{	jump = false;			
 				vel = 0;				
 			}
 		}					

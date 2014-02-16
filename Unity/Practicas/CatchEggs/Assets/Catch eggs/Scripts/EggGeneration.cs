60986cdaac602bd0e01c3014fced80c4f6fc96b9
Kanc <davidkanc@gmail.com>
2014-02-17 00:32:04 +0100

    unity

---
+++ Unity/Practicas/CatchEggs/Assets/Catch eggs/Scripts/EggGeneration.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class EggGeneration : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+
+		float elapsed = Time.time;
+
+		//print(elapsed);
+	
+	}
+}
---
+++ Unity/Practicas/CatchEggs/Assets/Catch eggs/Scripts/EggSettings.cs
@@ -0,0 +1,46 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class EggSettings : MonoBehaviour {
+
+	public float score;
+	public bool isGrouded;
+	public float timeToDissapear = 1;
+	float timeGrounded = 0;
+	float elapsedTimeGrounded;
+	
+	// Use this for initialization
+	void Start () 
+	{	
+		float eggType = Random.value;
+
+		if (eggType > 0.7f)
+		{	score = 5;
+			rigidbody.mass = 5;
+		}
+		else
+			score = 1;
+
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+		if (isGrouded)
+		{
+			if (timeGrounded == 0)
+				timeGrounded = Time.time;
+
+			elapsedTimeGrounded = Time.time - timeGrounded;
+
+			if (elapsedTimeGrounded >= timeToDissapear)
+				Destroy(gameObject);
+		}
+
+	}
+	void OnTriggerEnter (Collider other) 
+	{
+		if (other.gameObject.name == "ground")
+			isGrouded = true;
+	}
+}
